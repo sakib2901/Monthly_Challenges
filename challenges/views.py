411b0f568676f4fc30a68332beb35da6912e533c
BJIT <sakib.alim@bjitgroup.com>
2022-11-09 15:23:46 +0600

    Added views & urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def january(request):
+    return HttpResponse("Eat no junk food for the entire month!")
+
+def february(request):
+    return HttpResponse("Walk for at least 20 miles every day!")
